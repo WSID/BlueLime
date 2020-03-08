eefe0f86f58665d40617c3f826d3026223107bf2
Wissle <jongsome@gmail.com>
2020-03-08 22:22:10 +0900

    bluelime: Make C++ class for bluelime application

--- src/bluelime.cpp
+++ src/bluelime.cpp
@@ -10,175 +10,178 @@
 #include "bluelime.h"
 #include "chat_list_page.h"
 
-
 // ChatListPage -> ChatPage -> StickerPage
 // ^- AuthPage
 
-typedef struct appdata {
-	Evas_Object *win;
-	Evas_Object *conform;
-	Evas_Object *nf;
-	Eext_Circle_Surface *circle_surface;
-	ChatListPage chat_list_page;
-	std::unique_ptr<td::Client> td_client;
-} appdata_s;
+template <typename T, void (T::*function)(Evas_Object*)>
+class mf_2_cb {
+    public:
+    static void callback (void *data, Evas_Object *obj, void *einfo) {
+        T* self = (T*)data;
+        (self->*function)(obj);
+    }
+};
 
-static void
-win_delete_request_cb(void *data, Evas_Object *obj, void *event_info)
-{
-	ui_app_exit();
-}
+class app {
+    public:
+    Evas_Object *win;
+    Evas_Object *conform;
+    Evas_Object *naviframe;
+    Eext_Circle_Surface *circle_surface;
+    ChatListPage chat_list_page;
+    std::unique_ptr<td::Client> td_client;
 
-static void
-win_back_cb(void *data, Evas_Object *obj, void *event_info)
-{
-	appdata_s *ad = (appdata_s *)data;
-	/* Let window go to hide state. */
-	elm_win_lower(ad->win);
-}
+    app () : td_client (std::make_unique<td::Client>()) {
+        create_base_gui ();
+    }
 
-static void
-create_base_gui(appdata_s *ad)
-{
-	/* Window */
-	/* Create and initialize elm_win.
-	   elm_win is mandatory to manipulate window. */
-	ad->win = elm_win_util_standard_add(PACKAGE, PACKAGE);
-	elm_win_autodel_set(ad->win, EINA_TRUE);
+    ~ app () {
+        // Doing nothing here!
+        // td_client is auto-deleted.
+    }
 
-	if (elm_win_wm_rotation_supported_get(ad->win)) {
-		int rots[4] = { 0, 90, 180, 270 };
-		elm_win_wm_rotation_available_rotations_set(ad->win, (const int *)(&rots), 4);
-	}
+    // Callback for application lifecycle
+    void control(app_control_h app_control) {}
 
-	evas_object_smart_callback_add(ad->win, "delete,request", win_delete_request_cb, NULL);
-	eext_object_event_callback_add(ad->win, EEXT_CALLBACK_BACK, win_back_cb, ad);
+    void pause () {}
 
-	/* Conformant */
-	/* Create and initialize elm_conformant.
-	   elm_conformant is mandatory for base gui to have proper size
-	   when indicator or virtual keypad is visible. */
-	ad->conform = elm_conformant_add(ad->win);
-	elm_win_indicator_mode_set(ad->win, ELM_WIN_INDICATOR_SHOW);
-	elm_win_indicator_opacity_set(ad->win, ELM_WIN_INDICATOR_OPAQUE);
-	evas_object_size_hint_weight_set(ad->conform, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
-	elm_win_resize_object_add(ad->win, ad->conform);
-	evas_object_show(ad->conform);
+    void resume () {}
 
-	/* Naviframe */
-	ad->nf = elm_naviframe_add(ad->conform);
-	elm_object_content_set(ad->conform, ad->nf);
-	ad->circle_surface = eext_circle_surface_naviframe_add (ad->nf);
+    // Callback for UI application events
+    void ui_lang_changed (app_event_info_h event_info) {
+	    /*APP_EVENT_LANGUAGE_CHANGED*/
+	    char *locale = NULL;
+	    system_settings_get_value_string(SYSTEM_SETTINGS_KEY_LOCALE_LANGUAGE, &locale);
+	    elm_language_set(locale);
+	    free(locale);
+	    return;
+    }
 
-	/* Page 1 */
-	chat_list_page_init (& ad->chat_list_page, ad->nf, ad->circle_surface);
+    void ui_orient_changed (app_event_info_h event_info) {}
 
-	elm_naviframe_item_push(ad->nf, NULL, NULL, NULL, ad->chat_list_page.chat_genlist, "empty");
+    void ui_region_changed (app_event_info_h event_info) {}
 
-	/* Show window after base gui is set up */
-	evas_object_show(ad->win);
-}
+    void ui_low_battery (app_event_info_h event_info) {}
 
-static bool
-app_create(void *data)
-{
-	/* Hook to take necessary actions before main event loop starts
-		Initialize UI resources and application's data
-		If this function returns true, the main loop of application starts
-		If this function returns false, the application is terminated */
-	appdata_s *ad = (appdata_s *)data;
+    void ui_low_memory (app_event_info_h event_info) {}
 
-	ad->td_client =std::make_unique<td::Client>();
-	create_base_gui(ad);
+    private:
+    void create_base_gui () {
+        win = elm_win_util_standard_add(PACKAGE, PACKAGE);
+        elm_win_autodel_set(win, true);
 
-	return true;
-}
+        evas_object_smart_callback_add (win, "delete,request",
+            &mf_2_cb<app, &app::on_win_delete_request>::callback, this);
 
-static void
-app_control(app_control_h app_control, void *data)
-{
-	/* Handle the launch request. */
-}
+        eext_object_event_callback_add (win, EEXT_CALLBACK_BACK,
+            &mf_2_cb<app, &app::on_win_back>::callback, this);
 
-static void
-app_pause(void *data)
-{
-	/* Take necessary actions when application becomes invisible. */
-}
+	    /* Conformant */
+	    /* Create and initialize elm_conformant.
+	       elm_conformant is mandatory for base gui to have proper size
+	       when indicator or virtual keypad is visible. */
+	    conform = elm_conformant_add(win);
+	    elm_win_indicator_mode_set(win, ELM_WIN_INDICATOR_SHOW);
+	    elm_win_indicator_opacity_set(win, ELM_WIN_INDICATOR_OPAQUE);
+	    evas_object_size_hint_weight_set(conform, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+	    elm_win_resize_object_add(win, conform);
+	    evas_object_show(conform);
 
-static void
-app_resume(void *data)
-{
-	/* Take necessary actions when application becomes visible. */
-}
+	    /* Naviframe */
+	    naviframe = elm_naviframe_add(conform);
+	    elm_object_content_set(conform, naviframe);
+	    circle_surface = eext_circle_surface_naviframe_add (naviframe);
 
-static void
-app_terminate(void *data)
-{
-	appdata_s *ad = (appdata_s *)data;
+	    /* Page 1 */
+	    chat_list_page_init (& chat_list_page, naviframe, circle_surface);
 
-	ad->td_client.release();
-}
+	    elm_naviframe_item_push(naviframe, NULL, NULL, NULL, chat_list_page.chat_genlist, "empty");
 
-static void
-ui_app_lang_changed(app_event_info_h event_info, void *user_data)
-{
-	/*APP_EVENT_LANGUAGE_CHANGED*/
-	char *locale = NULL;
-	system_settings_get_value_string(SYSTEM_SETTINGS_KEY_LOCALE_LANGUAGE, &locale);
-	elm_language_set(locale);
-	free(locale);
-	return;
-}
+	    /* Show window after base gui is set up */
+	    evas_object_show(win);
+    }
 
-static void
-ui_app_orient_changed(app_event_info_h event_info, void *user_data)
-{
-	/*APP_EVENT_DEVICE_ORIENTATION_CHANGED*/
-	return;
-}
+    // Callback functions for windows
+    void on_win_delete_request(Evas_Object *obj) {
+        ui_app_exit();
+    }
 
-static void
-ui_app_region_changed(app_event_info_h event_info, void *user_data)
-{
-	/*APP_EVENT_REGION_FORMAT_CHANGED*/
-}
+    void on_win_back (Evas_Object *obj) {
+        elm_win_lower (win);
+    }
 
-static void
-ui_app_low_battery(app_event_info_h event_info, void *user_data)
-{
-	/*APP_EVENT_LOW_BATTERY*/
-}
 
-static void
-ui_app_low_memory(app_event_info_h event_info, void *user_data)
-{
-	/*APP_EVENT_LOW_MEMORY*/
-}
-
+};
 
 int
 main(int argc, char *argv[])
 {
-	appdata_s ad = {0,};
+    app *ap = nullptr;
 	int ret = 0;
 
 	ui_app_lifecycle_callback_s event_callback = {0,};
 	app_event_handler_h handlers[5] = {NULL, };
 
-	event_callback.create = app_create;
-	event_callback.terminate = app_terminate;
-	event_callback.pause = app_pause;
-	event_callback.resume = app_resume;
-	event_callback.app_control = app_control;
+    event_callback.create = [] (void *data) {
+        data = (void*) new app();
+        return true;
+    };
 
-	ui_app_add_event_handler(&handlers[APP_EVENT_LOW_BATTERY], APP_EVENT_LOW_BATTERY, ui_app_low_battery, &ad);
-	ui_app_add_event_handler(&handlers[APP_EVENT_LOW_MEMORY], APP_EVENT_LOW_MEMORY, ui_app_low_memory, &ad);
-	ui_app_add_event_handler(&handlers[APP_EVENT_DEVICE_ORIENTATION_CHANGED], APP_EVENT_DEVICE_ORIENTATION_CHANGED, ui_app_orient_changed, &ad);
-	ui_app_add_event_handler(&handlers[APP_EVENT_LANGUAGE_CHANGED], APP_EVENT_LANGUAGE_CHANGED, ui_app_lang_changed, &ad);
-	ui_app_add_event_handler(&handlers[APP_EVENT_REGION_FORMAT_CHANGED], APP_EVENT_REGION_FORMAT_CHANGED, ui_app_region_changed, &ad);
+    event_callback.terminate = [] (void *data) {
+        if (data != nullptr) {
+            delete (app*)data;
+            data = nullptr;
+        }
+    };
 
-	ret = ui_app_main(argc, argv, &event_callback, &ad);
+    event_callback.pause = [] (void *data) {
+        static_cast<app*>(data)->pause();
+    };
+
+    event_callback.resume = [] (void *data) {
+        static_cast<app*>(data)->resume();
+    };
+
+    event_callback.app_control = [] (app_control_h control, void *data) {
+        static_cast<app*>(data)->control (control);
+    };
+
+    ui_app_add_event_handler(
+        &handlers[APP_EVENT_LOW_BATTERY],
+        APP_EVENT_LOW_BATTERY,
+        [] (app_event_info_h event_info, void *data) {
+            static_cast<app*>(data)->ui_low_battery(event_info);
+        }, &ap);
+
+    ui_app_add_event_handler(
+        &handlers[APP_EVENT_LOW_MEMORY],
+        APP_EVENT_LOW_MEMORY,
+        [] (app_event_info_h event_info, void *data) {
+            static_cast<app*>(data)->ui_low_memory(event_info);
+        }, &ap);
+
+    ui_app_add_event_handler(
+        &handlers[APP_EVENT_DEVICE_ORIENTATION_CHANGED],
+        APP_EVENT_DEVICE_ORIENTATION_CHANGED,
+        [] (app_event_info_h event_info, void *data) {
+            static_cast<app*>(data)->ui_orient_changed(event_info);
+        }, &ap);
+
+    ui_app_add_event_handler(
+        &handlers[APP_EVENT_LANGUAGE_CHANGED],
+        APP_EVENT_LANGUAGE_CHANGED,
+        [] (app_event_info_h event_info, void *data) {
+            static_cast<app*>(data)->ui_lang_changed(event_info);
+        }, &ap);
+
+    ui_app_add_event_handler(
+        &handlers[APP_EVENT_REGION_FORMAT_CHANGED],
+        APP_EVENT_REGION_FORMAT_CHANGED,
+        [] (app_event_info_h event_info, void *data) {
+            static_cast<app*>(data)->ui_region_changed(event_info);
+        }, &ap);
+
+	ret = ui_app_main(argc, argv, &event_callback, &ap);
 	if (ret != APP_ERROR_NONE) {
 		dlog_print(DLOG_ERROR, LOG_TAG, "app_main() is failed. err = %d", ret);
 	}
