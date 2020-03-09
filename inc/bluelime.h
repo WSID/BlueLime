08382eb77be547a5e887a2727d7d4da61ac6df22
Wissle <jongsome@gmail.com>
2020-03-09 23:07:18 +0900

    Moved class definition to header.

--- inc/bluelime.h
+++ inc/bluelime.h
@@ -1,11 +1,25 @@
 #ifndef __bluelime_H__
 #define __bluelime_H__
 
+// stdlib includes
+#include <memory>
+
+// Tizen includes
 #include <app.h>
-#include <Elementary.h>
 #include <system_settings.h>
-#include <efl_extension.h>
 #include <dlog.h>
+
+// Enlightenment / Tizen Extension includes
+#include <Elementary.h>
+#include <efl_extension.h>
+
+// tdlib includes
+#include <td/telegram/Client.h>
+#include <td/telegram/td_api.h>
+#include <td/telegram/td_api.hpp>
+
+// local include
+#include "chat_list_page.h"
 
 #ifdef  LOG_TAG
 #undef  LOG_TAG
@@ -15,5 +29,52 @@
 #if !defined(PACKAGE)
 #define PACKAGE "standalone.bluelime"
 #endif
+
+// using usage - this will reduce verbosity of telegram library
+using namespace td::td_api;
+
+class app {
+  public:
+    Evas_Object *win;
+    Evas_Object *conform;
+    Evas_Object *naviframe;
+    Eext_Circle_Surface *circle_surface;
+    std::shared_ptr<ChatListPage> chat_list_page;
+
+  private:
+    std::unique_ptr<td::Client> td_client;
+    uint64_t td_query_id;
+
+  public:
+    app ();
+
+    ~ app ();
+
+    // Callback for application lifecycle
+    void control(app_control_h app_control);
+
+    void pause ();
+
+    void resume ();
+
+    // Callback for UI application events
+    void ui_lang_changed (app_event_info_h event_info);
+
+    void ui_orient_changed (app_event_info_h event_info);
+
+    void ui_region_changed (app_event_info_h event_info);
+
+    void ui_low_battery (app_event_info_h event_info);
+
+    void ui_low_memory (app_event_info_h event_info);
+
+  private:
+    void create_base_gui ();
+
+    // Callback functions for windows
+    void on_win_delete_request(Evas_Object *obj);
+
+    void on_win_back (Evas_Object *obj);
+};
 
 #endif /* __bluelime_H__ */
--- src/bluelime.cpp
+++ src/bluelime.cpp
@@ -1,14 +1,5 @@
-// stdlib includes
-#include <memory>
-
-// tdlib includes
-#include <td/telegram/Client.h>
-#include <td/telegram/td_api.h>
-#include <td/telegram/td_api.hpp>
-
-// local includes
 #include "bluelime.h"
-#include "chat_list_page.h"
+
 
 // ChatListPage -> ChatPage -> StickerPage
 // ^- AuthPage
@@ -22,96 +13,86 @@ class mf_2_cb {
     }
 };
 
-class app {
-    public:
-    Evas_Object *win;
-    Evas_Object *conform;
-    Evas_Object *naviframe;
-    Eext_Circle_Surface *circle_surface;
-    std::shared_ptr<ChatListPage> chat_list_page;
-    std::unique_ptr<td::Client> td_client;
+app::app () :
+    td_client (std::make_unique<td::Client>()),
+    td_query_id(1LU)
+{
+    create_base_gui ();
+}
 
-    app () : td_client (std::make_unique<td::Client>()) {
-        create_base_gui ();
-    }
+app::~ app () {
+    // Doing nothing here!
+    // td_client is auto-deleted.
+}
 
-    ~ app () {
-        // Doing nothing here!
-        // td_client is auto-deleted.
-    }
+// Callback for application lifecycle
+void app::control(app_control_h app_control) {}
 
-    // Callback for application lifecycle
-    void control(app_control_h app_control) {}
+void app::pause () {}
 
-    void pause () {}
+void app::resume () {}
 
-    void resume () {}
+// Callback for UI application events
+void app::ui_lang_changed (app_event_info_h event_info) {
+    /*APP_EVENT_LANGUAGE_CHANGED*/
+    char *locale = NULL;
+    system_settings_get_value_string(SYSTEM_SETTINGS_KEY_LOCALE_LANGUAGE, &locale);
+    elm_language_set(locale);
+    free(locale);
+    return;
+}
 
-    // Callback for UI application events
-    void ui_lang_changed (app_event_info_h event_info) {
-	    /*APP_EVENT_LANGUAGE_CHANGED*/
-	    char *locale = NULL;
-	    system_settings_get_value_string(SYSTEM_SETTINGS_KEY_LOCALE_LANGUAGE, &locale);
-	    elm_language_set(locale);
-	    free(locale);
-	    return;
-    }
+void app::ui_orient_changed (app_event_info_h event_info) {}
 
-    void ui_orient_changed (app_event_info_h event_info) {}
+void app::ui_region_changed (app_event_info_h event_info) {}
 
-    void ui_region_changed (app_event_info_h event_info) {}
+void app::ui_low_battery (app_event_info_h event_info) {}
 
-    void ui_low_battery (app_event_info_h event_info) {}
+void app::ui_low_memory (app_event_info_h event_info) {}
 
-    void ui_low_memory (app_event_info_h event_info) {}
+void app::create_base_gui () {
+    win = elm_win_util_standard_add(PACKAGE, PACKAGE);
+    elm_win_autodel_set(win, true);
 
-    private:
-    void create_base_gui () {
-        win = elm_win_util_standard_add(PACKAGE, PACKAGE);
-        elm_win_autodel_set(win, true);
+    evas_object_smart_callback_add (win, "delete,request",
+        &mf_2_cb<app, &app::on_win_delete_request>::callback, this);
 
-        evas_object_smart_callback_add (win, "delete,request",
-            &mf_2_cb<app, &app::on_win_delete_request>::callback, this);
+    eext_object_event_callback_add (win, EEXT_CALLBACK_BACK,
+        &mf_2_cb<app, &app::on_win_back>::callback, this);
 
-        eext_object_event_callback_add (win, EEXT_CALLBACK_BACK,
-            &mf_2_cb<app, &app::on_win_back>::callback, this);
+    /* Conformant */
+    /* Create and initialize elm_conformant.
+       elm_conformant is mandatory for base gui to have proper size
+       when indicator or virtual keypad is visible. */
+    conform = elm_conformant_add(win);
+    elm_win_indicator_mode_set(win, ELM_WIN_INDICATOR_SHOW);
+    elm_win_indicator_opacity_set(win, ELM_WIN_INDICATOR_OPAQUE);
+    evas_object_size_hint_weight_set(conform, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+    elm_win_resize_object_add(win, conform);
+    evas_object_show(conform);
 
-	    /* Conformant */
-	    /* Create and initialize elm_conformant.
-	       elm_conformant is mandatory for base gui to have proper size
-	       when indicator or virtual keypad is visible. */
-	    conform = elm_conformant_add(win);
-	    elm_win_indicator_mode_set(win, ELM_WIN_INDICATOR_SHOW);
-	    elm_win_indicator_opacity_set(win, ELM_WIN_INDICATOR_OPAQUE);
-	    evas_object_size_hint_weight_set(conform, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
-	    elm_win_resize_object_add(win, conform);
-	    evas_object_show(conform);
+    /* Naviframe */
+    naviframe = elm_naviframe_add(conform);
+    elm_object_content_set(conform, naviframe);
+    circle_surface = eext_circle_surface_naviframe_add (naviframe);
 
-	    /* Naviframe */
-	    naviframe = elm_naviframe_add(conform);
-	    elm_object_content_set(conform, naviframe);
-	    circle_surface = eext_circle_surface_naviframe_add (naviframe);
+    /* Page 1 */
+    chat_list_page = std::make_shared <ChatListPage> (naviframe, circle_surface);
 
-	    /* Page 1 */
-	    chat_list_page = std::make_shared <ChatListPage> (naviframe, circle_surface);
+    elm_naviframe_item_push(naviframe, NULL, NULL, NULL, chat_list_page->chat_genlist, "empty");
 
-	    elm_naviframe_item_push(naviframe, NULL, NULL, NULL, chat_list_page->chat_genlist, "empty");
+    /* Show window after base gui is set up */
+    evas_object_show(win);
+}
 
-	    /* Show window after base gui is set up */
-	    evas_object_show(win);
-    }
+// Callback functions for windows
+void app::on_win_delete_request(Evas_Object *obj) {
+    ui_app_exit();
+}
 
-    // Callback functions for windows
-    void on_win_delete_request(Evas_Object *obj) {
-        ui_app_exit();
-    }
-
-    void on_win_back (Evas_Object *obj) {
-        elm_win_lower (win);
-    }
-
-
-};
+void app::on_win_back (Evas_Object *obj) {
+    elm_win_lower (win);
+}
 
 int
 main(int argc, char *argv[])
