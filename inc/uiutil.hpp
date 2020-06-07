d5286639c12a37ad9dea558cb1092142110ffa4f
Wissle <jongsome@gmail.com>
2020-06-07 23:12:10 +0900

    uiutil: Added ui_entry_popup, for get string value, by showing popup.

--- inc/uiutil.hpp
+++ inc/uiutil.hpp
@@ -28,6 +28,36 @@ class ui_entry_button {
   Evas_Object *button;
   Evas_Object *box;
   Evas_Object *entry;
+
+class ui_entry_popup {
+  public:
+  ui_entry_popup(Evas_Object* naviframe);
+
+  Evas_Object *get_popup();
+  Evas_Object *get_layout();
+  Evas_Object *get_entry();
+  Evas_Object *get_button1();
+
+  const char *get_title () const;
+  void set_title (const char *title);
+
+  const char *get_description () const;
+  void set_description (const char *description);
+
+  const char *get_text () const;
+  void set_text(const char *text);
+
+  const char *get_placeholder() const;
+  void set_placeholder(const char *text);
+
+  private:
+  Evas_Object *naviframe;
+  Evas_Object *popup;
+  Evas_Object *layout;
+  Evas_Object *box;
+  Evas_Object *label;
+  ui_entry_button subui_entry_button;
+  Evas_Object *button1;
 };
 
 #endif //UI_UTIL_H
--- src/uiutil.cpp
+++ src/uiutil.cpp
@@ -62,3 +62,93 @@ void ui_entry_button::callback_entry_changed (void *data, Evas_Object *object, v
       elm_object_part_text_get (object, "guide") :
       elm_entry_entry_get (object));
 }
+
+
+
+ui_entry_popup::ui_entry_popup(Evas_Object* naviframe) :
+  naviframe (naviframe),
+  popup (elm_popup_add(naviframe)),
+  layout (elm_layout_add (popup)),
+  box (elm_box_add(layout)),
+  label (elm_label_add (box)),
+  subui_entry_button (box, naviframe)
+{
+  // popup
+  elm_object_style_set (popup, "circle");
+  elm_object_content_set (popup, layout);
+
+  // layout
+  elm_layout_theme_set (layout, "layout", "popup", "content/circle/buttons1");
+  elm_object_content_set (layout, box);
+
+  // box
+  elm_box_pack_end (box, label);
+  elm_box_pack_end (box, subui_entry_button.get_button());
+  evas_object_show (box);
+
+  // label
+  evas_object_show (label);
+  evas_object_size_hint_weight_set (label, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+  evas_object_size_hint_align_set (label, EVAS_HINT_FILL, EVAS_HINT_FILL);
+  elm_label_line_wrap_set (label, ELM_WRAP_MIXED);
+
+  // popup button
+  Evas_Object *button = subui_entry_button.get_button ();
+  evas_object_show (button);
+  evas_object_size_hint_weight_set (button, EVAS_HINT_EXPAND, 0.0);
+  evas_object_size_hint_align_set (button, EVAS_HINT_FILL, EVAS_HINT_FILL);
+
+  // Action button 1
+  button1 = elm_button_add (popup);
+  elm_object_style_set (button1, "popup/circle/bottom");
+  elm_object_part_content_set (popup, "button1", button1);
+}
+
+Evas_Object *ui_entry_popup::get_popup () {
+  return popup;
+}
+
+Evas_Object *ui_entry_popup::get_layout () {
+  return layout;
+}
+
+Evas_Object *ui_entry_popup::get_entry () {
+  return subui_entry_button.get_entry();
+}
+
+Evas_Object *ui_entry_popup::get_button1 () {
+  return button1;
+}
+
+const char *ui_entry_popup::get_title () const {
+  return elm_object_part_text_get (layout, "elm.text.titlte");
+}
+
+void ui_entry_popup::set_title (const char *title) {
+  elm_object_part_text_set (layout, "elm.text.title", title);
+}
+
+const char *ui_entry_popup::get_description () const {
+  return elm_object_text_get (label);
+}
+
+void ui_entry_popup::set_description (const char *description) {
+  elm_object_text_set (label, description);
+}
+
+const char *ui_entry_popup::get_text () const {
+  return subui_entry_button.get_text ();
+}
+
+void ui_entry_popup::set_text(const char *text) {
+  subui_entry_button.set_text (text);
+}
+
+const char *ui_entry_popup::get_placeholder () const {
+  return subui_entry_button.get_placeholder ();
+}
+
+void ui_entry_popup::set_placeholder (const char *placeholder) {
+  subui_entry_button.set_placeholder (placeholder);
+}
+
