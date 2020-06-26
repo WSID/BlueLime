f276a7b41d9335e367dc68c6f121cfdbf6863256
Wissle <jongsome@gmail.com>
2020-06-27 00:38:36 +0900

    uiutil: Added ui_agreement_popup for long text display.

--- inc/uiutil.hpp
+++ inc/uiutil.hpp
@@ -2,10 +2,20 @@
 #define __UI_UTIL_H__
 
 // C++ stdlib include
+#include <functional>
 #include <string>
+#include <sstream>
 
 // Enlightenment includes
 #include <Elementary.h>
+
+// EFL Extension includes
+#include <efl_extension.h>
+
+// Telegram includes
+#include <td/telegram/td_api.h>
+#include <td/telegram/td_api.hpp>
+
 
 class ui_entry_button {
   public:
@@ -28,6 +38,7 @@ class ui_entry_button {
   Evas_Object *button;
   Evas_Object *box;
   Evas_Object *entry;
+};
 
 class ui_entry_popup {
   public:
@@ -60,4 +71,48 @@ class ui_entry_popup {
   Evas_Object *button1;
 };
 
+class ui_agreement_popup {
+  public:
+  ui_agreement_popup (Evas_Object* naviframe, Eext_Circle_Surface* circle_surface);
+
+  Evas_Object *get_popup();
+
+  void set_description (const char *description);
+
+  void set_content (const char *content);
+
+  std::function<void()> on_agree;
+
+  private:
+  static void on_button_clicked (void *data, Evas_Object *object, void *event_info);
+
+  static void on_link_clicked (void *data, Evas_Object *object, void *event_info);
+
+  Evas_Object *naviframe;
+  Evas_Object *scroll;
+  Evas_Object *cscroll;
+  Evas_Object *box;
+  Evas_Object *label_descr;
+  Evas_Object *entry_content;
+  Evas_Object *button;
+};
+
+/**
+ * decorate_string:
+ *
+ * Decorates string for EFL formatted string, from telegram's formatted text.
+ *
+ * @formatted_text: (in): A Telegram's formatted text.
+ *
+ * @returns: A decorated string with tags.
+ */
+std::string decorate_string (const td::td_api::formattedText& formatted_text);
+
+std::ostream& decorate_in (
+    std::ostream& os,
+    const td::td_api::TextEntityType& entity_type);
+
+std::ostream& decorate_out (
+    std::ostream& os,
+    const td::td_api::TextEntityType& entity_type);
 #endif //UI_UTIL_H
--- src/uiutil.cpp
+++ src/uiutil.cpp
@@ -1,5 +1,10 @@
 #include "uiutil.hpp"
 
+#include <map>
+
+#include <app.h>
+#include <dlog.h>
+
 ui_entry_button::ui_entry_button(Evas_Object* parent, Evas_Object* naviframe) :
   naviframe (naviframe),
   button (elm_button_add(parent)),
@@ -152,3 +157,277 @@ void ui_entry_popup::set_placeholder (const char *placeholder) {
   subui_entry_button.set_placeholder (placeholder);
 }
 
+
+
+ui_agreement_popup::ui_agreement_popup(Evas_Object* naviframe, Eext_Circle_Surface* circle_surface)
+  : naviframe (naviframe),
+    scroll (elm_scroller_add (naviframe)),
+    cscroll (eext_circle_object_scroller_add (scroll, circle_surface)),
+    box (elm_box_add (scroll)),
+    label_descr (elm_label_add(box)),
+    entry_content (elm_entry_add(box)),
+    button (elm_button_add (box))
+{
+  // scroll
+  elm_scroller_policy_set (scroll,
+      ELM_SCROLLER_POLICY_OFF,
+      ELM_SCROLLER_POLICY_OFF);
+  elm_object_content_set (scroll, box);
+
+  // cscroll
+  eext_circle_object_scroller_policy_set (cscroll,
+      ELM_SCROLLER_POLICY_OFF,
+      ELM_SCROLLER_POLICY_AUTO);
+  evas_object_show (cscroll);
+
+  // box
+  elm_box_padding_set (box, 20, 20);
+  elm_box_pack_end (box, label_descr);
+  elm_box_pack_end (box, entry_content);
+  elm_box_pack_end (box, button);
+  evas_object_show (box);
+
+  // label_descr
+  evas_object_size_hint_padding_set (label_descr, 40, 40, 160, 0);
+  evas_object_size_hint_align_set (label_descr, EVAS_HINT_FILL, EVAS_HINT_FILL);
+  evas_object_size_hint_weight_set (label_descr, EVAS_HINT_EXPAND, 0);
+  elm_label_line_wrap_set (label_descr, ELM_WRAP_MIXED);
+  evas_object_show (label_descr);
+
+  // entry_content
+  evas_object_size_hint_padding_set (entry_content, 40, 40, 0, 0);
+  evas_object_size_hint_align_set (entry_content, EVAS_HINT_FILL, EVAS_HINT_FILL);
+  evas_object_size_hint_weight_set (entry_content, EVAS_HINT_EXPAND, EVAS_HINT_EXPAND);
+  elm_entry_line_wrap_set (entry_content, ELM_WRAP_MIXED);
+  elm_entry_editable_set(entry_content, EINA_FALSE);
+  evas_object_smart_callback_add (entry_content, "anchor,clicked", &ui_agreement_popup::on_link_clicked, this);
+  evas_object_show (entry_content);
+
+  // button
+  evas_object_size_hint_align_set (button, EVAS_HINT_FILL, EVAS_HINT_FILL);
+  evas_object_size_hint_weight_set (button, EVAS_HINT_EXPAND, 0);
+  elm_object_text_set (button, "accept");
+  elm_object_style_set (button, "bottom");
+  evas_object_smart_callback_add (button, "clicked", &ui_agreement_popup::on_button_clicked, &on_agree);
+  evas_object_show (button);
+}
+
+Evas_Object *ui_agreement_popup::get_popup () {
+  return scroll;
+}
+
+void ui_agreement_popup::set_description (const char *description) {
+  elm_object_text_set (label_descr, description);
+}
+
+void ui_agreement_popup::set_content (const char *content) {
+  elm_object_text_set (entry_content, content);
+}
+
+void ui_agreement_popup::on_button_clicked (void *data, Evas_Object *object, void *event_info) {
+  std::function<void ()> &callable = *(std::function<void ()>*)data;
+
+  if (callable) {
+    callable ();
+  }
+}
+
+void ui_agreement_popup::on_link_clicked (void *data, Evas_Object *object, void *event_info) {
+  Elm_Entry_Anchor_Info *anchor_info = (Elm_Entry_Anchor_Info*)event_info;
+  dlog_print(DLOG_VERBOSE, "bluelime", "clicked anchor: %s", anchor_info->name);
+
+  // Launch web browser.
+  app_control_h app_control;
+  app_control_create (&app_control);
+  app_control_set_operation (app_control, APP_CONTROL_OPERATION_VIEW);
+  app_control_set_uri(app_control, anchor_info->name);
+  app_control_send_launch_request (app_control, nullptr, nullptr);
+  app_control_destroy (app_control);
+}
+
+
+std::string decorate_string (const td::td_api::formattedText& formatted_text) {
+  std::multimap<int, int> map_pos_idx;
+  // A mapping from pos, to index
+  // - position on formatted string.
+  // - index to format text entry.
+
+  // Build up break point maps.
+  int n = formatted_text.entities_.size();
+  for (int i = 0; i < n; ++i) {
+    const td::td_api::object_ptr<td::td_api::textEntity> &entity = formatted_text.entities_[i];
+    map_pos_idx.emplace(entity->offset_, i + 1);
+  }
+
+  for (int i = n - 1; 0 <= i; --i) {
+    const td::td_api::object_ptr<td::td_api::textEntity> &entity = formatted_text.entities_[i];
+    map_pos_idx.emplace(entity->offset_ + entity->length_, -i - 1);
+  }
+
+  // Iterate over breakage.
+  std::ostringstream result_builder;
+  std::multimap<int, int>::const_iterator iter;
+  std::multimap<int, int>::const_iterator end;
+  iter = map_pos_idx.cbegin();
+  end = map_pos_idx.cend();
+
+
+
+  int head = 0;
+  while (iter != end) {
+    int nhead = iter->first;
+
+    if (head < nhead) {
+      result_builder << formatted_text.text_.substr (head, nhead - head);
+      head = nhead;
+    }
+
+    if (iter->second < 0) {
+      int idx = (-iter->second) - 1;
+      decorate_out (result_builder, *formatted_text.entities_[idx]->type_.get());
+    }
+
+    else if (0 < iter->second) {
+      int idx = iter->second - 1;
+      decorate_in (result_builder, *formatted_text.entities_[idx]->type_.get());
+    }
+
+    iter ++;
+  }
+
+  result_builder << formatted_text.text_.substr (head);
+
+  // Replace newline with <br>
+  std::ostringstream br_result_builder;
+  std::string decorated = result_builder.str();
+  std::string::size_type br_head = 0;
+  std::string::size_type br_nhead = decorated.find('\n', 0);
+  while (br_nhead != std::string::npos) {
+    br_result_builder << decorated.substr(br_head, br_nhead - br_head) << "<br>";
+    br_head = br_nhead + 1;
+    br_nhead = decorated.find('\n', br_head);
+  }
+  br_result_builder << decorated.substr(br_head);
+
+  return br_result_builder.str();
+}
+
+std::ostream& decorate_in (
+    std::ostream& os,
+    const td::td_api::TextEntityType& entity_type)
+{
+  // TODO: We can add custom tag for EFL to process, through theme set.
+  // TODO: Some attribute may be used to make links!
+  switch (entity_type.get_id()) {
+  case td::td_api::textEntityTypeBold::ID:
+    return os << "<font_weight=bold>";
+
+  case td::td_api::textEntityTypeBotCommand::ID:
+    return os << "<font=Monospace>";
+
+  case td::td_api::textEntityTypeCashtag::ID:
+    return os << "<font_width=expanded>"; // <cashtag>
+
+  case td::td_api::textEntityTypeCode::ID:
+    return os << "<font=Monospace>";
+
+  case td::td_api::textEntityTypeEmailAddress::ID:
+    return os << "<font_style=Italic>"; // <a href="mailto://...">
+
+  case td::td_api::textEntityTypeHashtag::ID:
+    return os << "<link>"; // <hashtag>
+
+  case td::td_api::textEntityTypeItalic::ID:
+    return os << "<em>";
+
+  case td::td_api::textEntityTypeMention::ID:
+    return os << "<keyword>"; // <mention>
+
+  case td::td_api::textEntityTypeMentionName::ID:
+    return os << "<keyword>"; // <mentionname>
+
+  case td::td_api::textEntityTypePhoneNumber::ID:
+    return os << "<number>"; // <phonenumber>
+
+  case td::td_api::textEntityTypePre::ID:
+    return os << "<code>"; // <pre>
+
+  case td::td_api::textEntityTypePreCode::ID:
+    return os << "<code>"; // <pre> code
+
+  case td::td_api::textEntityTypeStrikethrough::ID:
+    return os << "<link>"; // strikethrough
+
+  case td::td_api::textEntityTypeTextUrl::ID:
+    {
+      const td::td_api::textEntityTypeTextUrl& entity_text_url =
+        static_cast<const td::td_api::textEntityTypeTextUrl&>(entity_type);
+      return os << "<underline=on><color=#8CF><a href=" << entity_text_url.url_ << ">";
+    }
+
+  case td::td_api::textEntityTypeUnderline::ID:
+    return os << "<link>"; // Underline
+
+  case td::td_api::textEntityTypeUrl::ID:
+    return os << "<link>"; // Link
+  }
+
+  return os;
+}
+
+std::ostream& decorate_out (
+    std::ostream& os,
+    const td::td_api::TextEntityType& entity_type)
+{
+  switch (entity_type.get_id()) {
+  case td::td_api::textEntityTypeBold::ID:
+    return os << "</font_weight>";
+
+  case td::td_api::textEntityTypeBotCommand::ID:
+    return os << "</font>";
+
+  case td::td_api::textEntityTypeCashtag::ID:
+    return os << "</font_width>"; // <cashtag>
+
+  case td::td_api::textEntityTypeCode::ID:
+    return os << "</font>";
+
+  case td::td_api::textEntityTypeEmailAddress::ID:
+    return os << "</font_style>"; // <a href="mailto://...">
+
+  case td::td_api::textEntityTypeHashtag::ID:
+    return os << "</link>"; // <hashtag>
+
+  case td::td_api::textEntityTypeItalic::ID:
+    return os << "</em>";
+
+  case td::td_api::textEntityTypeMention::ID:
+    return os << "</keyword>"; // <mention>
+
+  case td::td_api::textEntityTypeMentionName::ID:
+    return os << "</keyword>"; // <mentionname>
+
+  case td::td_api::textEntityTypePhoneNumber::ID:
+    return os << "</number>"; // <phonenumber>
+
+  case td::td_api::textEntityTypePre::ID:
+    return os << "</code>"; // <pre>
+
+  case td::td_api::textEntityTypePreCode::ID:
+    return os << "</code>"; // <pre> code
+
+  case td::td_api::textEntityTypeStrikethrough::ID:
+    return os << "</link>"; // strikethrough
+
+  case td::td_api::textEntityTypeTextUrl::ID:
+    return os << "</a></color></underline>";
+
+  case td::td_api::textEntityTypeUnderline::ID:
+    return os << "</link>"; // Underline
+
+  case td::td_api::textEntityTypeUrl::ID:
+    return os << "</link>"; // Link
+  }
+
+  return os;
+}
